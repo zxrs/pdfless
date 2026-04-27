040dcd49b5cd27f2c9185b35881fe6d2105cf06d
zxrs <tanashin@bj.wakwak.com>
2026-04-27 16:18:21 +0900

    render successfully

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::cell::RefCell;
+
 use anyhow::Result;
-use icy_sixel::SixelImage;
 use pdf_thumb::{ImageFormat, Options, PdfDoc};
+use ratatui::{DefaultTerminal, Frame, layout::Rect};
+use ratatui_image::{
+    Image,
+    protocol::{Protocol, sixel::Sixel},
+};
+
+thread_local! {
+    static SIXEL: RefCell<Option<String>> = RefCell::new(None);
+}
+
+struct App {
+    image: Protocol,
+}
 
 fn main() -> Result<()> {
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> Result<()> {
     let pdf = PdfDoc::open(r"test.pdf")?;
     let thumb = pdf.thumb_with_options(Options {
         width: 800,
@@ -10,11 +29,33 @@ fn main() -> Result<()> {
         ..Default::default()
     })?;
     let image = image::load_from_memory_with_format(&thumb, image::ImageFormat::Bmp)?;
-    let width = image.width() as usize;
-    let height = image.height() as usize;
-    let rgba = image.to_rgba8().to_vec();
-    let sixel_image = SixelImage::try_from_rgba(rgba, width, height)?;
-    let sixel = sixel_image.encode()?;
-    print!("{sixel}");
+    let width = image.width() as u16;
+    let height = image.height() as u16;
+
+    // print!("{sixel}");
+    let mut app = App {
+        image: Protocol::Sixel(Sixel::new(
+            image,
+            Rect {
+                x: 0,
+                y: 0,
+                width: 130,
+                height: 50,
+            },
+            false,
+        )?),
+    };
+
+    loop {
+        terminal.draw(|f| render(f, &mut app))?;
+        if crossterm::event::read()?.is_key_press() {
+            break;
+        }
+    }
     Ok(())
+}
+
+fn render(frame: &mut Frame<'_>, app: &mut App) {
+    let image = Image::new(&mut app.image);
+    frame.render_widget(image, frame.area());
 }
