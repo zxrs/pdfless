bd2e39cfb3a29f73ea8e6520597e9373c03ea9af
zxrs <tanashin@bj.wakwak.com>
2026-04-27 16:54:27 +0900

    calculate image size

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::cell::RefCell;
-
 use anyhow::Result;
 use pdf_thumb::{ImageFormat, Options, PdfDoc};
-use ratatui::{DefaultTerminal, Frame, layout::Rect};
+use ratatui::{DefaultTerminal, Frame, layout::Rect, prelude::Backend};
 use ratatui_image::{
+    FilterType::Lanczos3,
     Image,
     protocol::{Protocol, sixel::Sixel},
 };
-
-thread_local! {
-    static SIXEL: RefCell<Option<String>> = RefCell::new(None);
-}
 
 struct App {
     image: Protocol,
@@ -22,25 +17,30 @@ fn main() -> Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> Result<()> {
-    let pdf = PdfDoc::open(r"test.pdf")?;
+    let size = terminal.size()?;
+
+    let pdf = PdfDoc::open(r"v.pdf")?;
     let thumb = pdf.thumb_with_options(Options {
-        width: 800,
         format: ImageFormat::Bmp,
         ..Default::default()
     })?;
     let image = image::load_from_memory_with_format(&thumb, image::ImageFormat::Bmp)?;
-    let width = image.width() as u16;
-    let height = image.height() as u16;
+    let image = if image.width() > image.height() {
+        // TODO: these factor "10" should be calculated...
+        image.resize(size.width as u32 * 10, size.width as u32 * 10, Lanczos3)
+    } else {
+        // TODO: these factor "20" should be calculated...
+        image.resize(size.height as u32 * 20, size.height as u32 * 20, Lanczos3)
+    };
 
-    // print!("{sixel}");
     let mut app = App {
         image: Protocol::Sixel(Sixel::new(
             image,
             Rect {
                 x: 0,
                 y: 0,
-                width: 130,
-                height: 50,
+                width: size.width,
+                height: size.height,
             },
             false,
         )?),
